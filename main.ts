d749ce7302f0870fd22274c2b54ab69d45c065b2
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2023-09-21 19:16:15 -0400

    typescript version

--- main.ts
+++ main.ts
@@ -5,10 +5,12 @@
  * This program shows a temperature of a microprocessor
 */
 
+let t = input.temperature()
+
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
 input.onButtonPressed(Button.A, function () {
   input.temperature()
-  basic.showString('The temperature is:' + input.temperature().toString() + 'C')
+    basic.showString('The temperature is:' + (t.toString()) + 'C')
 })
