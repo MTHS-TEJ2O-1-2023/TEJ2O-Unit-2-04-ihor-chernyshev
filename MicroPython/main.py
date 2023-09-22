3bc861660d13ac20a64cbc89d1d095c7ded992ab
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2023-09-22 23:40:46 +0000

    python version

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,13 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Ihor Chernyshev
+Created on: Sep 2023
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
+display.clear()
+display.show(Image.HAPPY)
 
-
-display.scroll("Hello, World!")
+while True:
+    if button_a.was_pressed():
+        display.scroll(temperature())
